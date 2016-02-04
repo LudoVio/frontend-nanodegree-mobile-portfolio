af9cfdd40d80bb13317f994554ee388c2cb8c222
Ludovic Viaud <ludovic.viaud@gmail.com>
2016-02-04 03:01:11 -0300

    feat: replace querySelector by getElementById

--- app/views/js/main.js
+++ app/views/js/main.js
@@ -403,18 +403,19 @@ var pizzaElementGenerator = function (i) {
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
 var resizePizzas = function (size) {
     window.performance.mark("mark_start_resize");   // User Timing API function
+    var pizzaSizeLabel = document.getElementById("pizzaSize");
 
     // Changes the value for the size of the pizza above the slider
     function changeSliderLabel(size) {
         switch (size) {
             case "1":
-                document.querySelector("#pizzaSize").innerHTML = "Small";
+                pizzaSizeLabel.innerHTML = "Small";
                 return;
             case "2":
-                document.querySelector("#pizzaSize").innerHTML = "Medium";
+                pizzaSizeLabel.innerHTML = "Medium";
                 return;
             case "3":
-                document.querySelector("#pizzaSize").innerHTML = "Large";
+                pizzaSizeLabel.innerHTML = "Large";
                 return;
             default:
                 console.log("bug in changeSliderLabel");
